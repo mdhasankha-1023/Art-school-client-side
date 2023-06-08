cc007d52f622bb31c00b8633144c2def32c7c39e
hasankhan-123 <hasankha1023@gmail.com>
2023-06-08 06:52:18 +0600

    Navbar added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Routes/Route.jsx
@@ -0,0 +1,11 @@
+import {createBrowserRouter} from "react-router-dom";
+import Main from "../Layout/Main";
+
+  const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>
+    }
+  ])
+
+  export default router;
