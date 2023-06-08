c1a1a81fb9192362a73814715adf5738a4164dd1
hasankhan-123 <hasankha1023@gmail.com>
2023-06-08 06:12:56 +0600

    Art-schoolproject config setup complited

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-center text-3sl font-bold'>This is button group</h1>
+        <button className="btn">Button</button>
+      </div>
+    </>
+  )
+}
+
+export default App
